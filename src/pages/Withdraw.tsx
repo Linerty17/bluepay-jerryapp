c0d291324a89a9bc2da8199027be6a8f7200b8e3
Linerty17 <187655124+Linerty17@users.noreply.github.com>
2025-06-18 21:19:06 +0100

    Update Withdraw.tsx

--- src/pages/Withdraw.tsx
+++ src/pages/Withdraw.tsx
@@ -73,7 +73,7 @@ const Withdraw = () => {
     }
 
     // Validate BPC code
-    if (bpcCode !== "02476814") {
+    if (bpcCode !== "65441104") {
       toast({
         variant: "destructive",
         description: "Invalid BPC code. Please enter a valid code.",
